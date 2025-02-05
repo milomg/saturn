4088a4d56949d66df5804e763410d5ed47792741
Milo <milomg4@gmail.com>
2025-02-05 00:55:42 -0500

    diff with yjs on remount to resync

--- src/utils/lezer-mips/collab.ts
+++ src/utils/lezer-mips/collab.ts
@@ -3,8 +3,10 @@ import { WebrtcProvider } from 'y-webrtc'
 import { createState, EditorTab, Tabs } from '../tabs'
 import { tabsState } from '../../state/state'
 import { markRaw } from 'vue'
-import { Compartment, Extension } from '@codemirror/state'
+import { ChangeSpec, Compartment, Extension } from '@codemirror/state'
 import { yCollab } from 'y-codemirror.next'
+import { EditorView, ViewPlugin } from '@codemirror/view'
+import { diff } from 'fast-myers-diff'
 
 export const usercolors = [
   { color: '#30bced', light: '#30bced33' },
@@ -27,6 +29,31 @@ const random = Math.floor(Math.random() * usercolors.length)
 // select a random color for this user
 export const userColor = usercolors[random]
 
+const syncPlugin = (ytext: Y.Text) =>
+  ViewPlugin.fromClass(
+    class {
+      constructor(public view: EditorView) {
+        if (view.state.doc.toString() != ytext.toString()) {
+          const ystr = ytext.toString()
+          const mydiff = diff(view.state.doc.toString(), ystr)
+          const changes: ChangeSpec[] = []
+          for (const [sx, ex, sy, ey] of mydiff) {
+            changes.push({
+              from: sx,
+              to: ex,
+              insert: ystr.slice(sy, ey),
+            })
+          }
+          queueMicrotask(() => {
+            view.dispatch({
+              changes: changes,
+            })
+          })
+        }
+      }
+    },
+  )
+
 export const hostYTab = (tab: EditorTab) => {
   const ydoc = new Y.Doc()
   const ytext = ydoc.getText('codemirror')
@@ -46,9 +73,10 @@ export const hostYTab = (tab: EditorTab) => {
 
   console.log(tab.uuid)
 
-  return collabCompartment.reconfigure(
+  return collabCompartment.reconfigure([
     yCollab(ytext, provider.awareness, { undoManager }),
-  )
+    syncPlugin(ytext),
+  ])
 }
 
 export const joinYTab = (editor: Tabs, join: string): EditorTab => {
@@ -71,6 +99,7 @@ export const joinYTab = (editor: Tabs, join: string): EditorTab => {
 
   const state = createState(editor, id, content, true, [
     yCollab(ytext, provider.awareness, { undoManager }),
+    syncPlugin(ytext),
   ])
 
   const tab: EditorTab = {
